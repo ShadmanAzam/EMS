391e16c75b417e55010432d2dc1ccb1ae042cda4
Syed Shadman Azam <103278122+ShadmanAzam@users.noreply.github.com>
2025-09-01 21:45:30 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'Ems',
 		'USER': 'root',
-		'PASSWORD': 'Shagufta@123',
+		'PASSWORD': '',
 		'HOST':'localhost',
 		'PORT':'3306',
 	}
